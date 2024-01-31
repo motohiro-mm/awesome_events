6bd9d18625fa87d95aa5402af0886b8dc7d35875
motohiro-mm <polarbear.dx@gmail.com>
2024-01-31 17:07:39 +0900

    first

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,8 @@
+class WelcomeController < ApplicationController
+  skip_before_action :authenticate
+
+  def index
+    @events = Event.page(params[:page]).per(10).where("start_at > ?", Time.zone.now).order(:start_at)
+    raise StandardError
+  end
+end

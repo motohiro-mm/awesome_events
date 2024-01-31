329433a31f1f8064f246d13506a5ffb43c453a8a
motohiro-mm <polarbear.dx@gmail.com>
2024-01-31 17:17:03 +0900

    コントローラからraiseを削除

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,5 @@ class WelcomeController < ApplicationController
 
   def index
     @events = Event.page(params[:page]).per(10).where("start_at > ?", Time.zone.now).order(:start_at)
-    raise StandardError
   end
 end
